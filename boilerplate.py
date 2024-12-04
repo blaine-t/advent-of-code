2c272fc28fd4ee4ce12f3eae62fc3d5ecc63ec8a
blaine-t <blaine-t@bathost.net>
2024-12-03 19:50:58 -0600

    Add boilerplate for Python

---
+++ boilerplate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env pypy3
+from lib import *
+import math
+import re
+from pprint import pprint
+from sys import argv
+
+setDebug(True)
+setTest(False)
+
+sum = 0
+list1 = []
+list2 = []
+list3 = []
+for line in readFileLines():
+    ints = reInts(line)
+
+# list1.sort()
+# list2.sort()
+# list3.sort()
+
+# for i, item in enumerate(list1):
+#     sum +=  abs(item - list2[i])
+
+pprint(sum)
