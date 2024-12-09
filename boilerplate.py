aaada03537f6f2eb9e6198c6e7480719b2d71e09
blaine-t <blaine-t@bathost.net>
2024-12-09 00:21:03 -0600

    Update Python boilerplate

--- boilerplate.py
+++ boilerplate.py
@@ -8,14 +8,23 @@ from sys import argv
 setDebug(True)
 setTest(False)
 
-result = 0
+res = 0
 list1 = []
 list2 = []
 list3 = []
+lines = readFileLines()
+xLength = len(lines[0])
+yLength = len(lines)
+imap = {}
 regex = '^\d+'
-for line in readFileLines():
-    regResList = re.findall(regex, line)
-    ints = reInts(line)
+for i, line in enumerate(lines):
+    print(i, line)
+
+
+for line in lines:
+    for char in line:
+        if char == '#':
+            res += 1
 
 # list1.sort()
 # list2.sort()
@@ -24,4 +33,4 @@ for line in readFileLines():
 # for i, item in enumerate(list1):
 #     sum +=  abs(item - list2[i])
 
-pprint(result)
+pprint(res)
